d6cee32bf22b2229b7fb3aa40f53a62ddc7f3263
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-01-28 16:12:24 -0600

    Create fetchLang.js

---
+++ translate-api/fetchLang.js
@@ -0,0 +1,52 @@
+        const Str = x => {
+            try {
+                return String(x);
+            } catch (e) {
+                return String(e);
+            }
+        };
+
+        const stringify = x => {
+            try {
+                return JSON.stringify(x);
+            } catch {
+                return Str(x);
+            }
+        };
+
+
+        const url = 'https://script.google.com/macros/s/AKfycbzBFvLSewHPfKa0aemNWTO6PvXGhLzolyJkWlKVylPeCQNQIT3GoygE4k6pvTOYXoHC/exec';
+
+
+        const fetchText = async (...args) => (await lfetch(...args)).text();
+
+         const langCache = {};
+        async function fixText(text) {
+           if(!text?.trim?.()){
+            return text;
+           }
+            text = text.trim();
+            let out;
+            try{
+            if (localStorage.getItem(text)) return localStorage.getItem(text);
+            if(langCache[text])return JSON.parse(await langCache[text]).textOut;
+            const payload = { text };
+            payload.sourceLang = 'detect';
+            payload.targetLang = 'en';
+            langCache[text] = (fetchText(`${url}`, {
+                method: "POST",
+                body: encodeURIComponent(stringify(payload))
+            }));
+             responsePayload = JSON.parse(await langCache[text]);
+             out = responsePayload.textOut;
+             //console.log({text},{responsePayload});
+            }catch(e){
+             console.warn(e);
+            }
+            if (out?.trim?.() && (out?.trim?.() !== '#ERROR!') && out != text) {
+                localStorage.setItem(text, out);
+            }else{
+             delete langCache[text];
+            }
+            return out || text;
+        }
